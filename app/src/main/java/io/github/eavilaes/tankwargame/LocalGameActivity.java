11283ef098e47b434ca5f040801f79a8e0aa3f61
Eric <ericavilaescudero@gmail.com>
2020-03-18 19:41:31 +0000

    Added scoreboard for each player

--- app/src/main/java/io/github/eavilaes/tankwargame/LocalGameActivity.java
+++ app/src/main/java/io/github/eavilaes/tankwargame/LocalGameActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
@@ -19,6 +20,8 @@ public class LocalGameActivity extends AppCompatActivity {
     ConstraintLayout layout;
 
     private static final float bulletSpeedMultiplier = 50.0f;
+    private static final int NANOS_TO_SECONDS = 1000000000;
+    private static final int MAX_BULLET_TIME = 3; //seconds
     Tank player1, player2;
     Point size = new Point();
     private int W;
@@ -39,8 +42,8 @@ public class LocalGameActivity extends AppCompatActivity {
         H = size.y;
 
         //Create tanks and set rotations
-        player1 = new Tank((ImageView)findViewById(R.id.tank_player));
-        player2 = new Tank((ImageView)findViewById(R.id.tank_player2));
+        player1 = new Tank((ImageView)findViewById(R.id.tank_player),1);
+        player2 = new Tank((ImageView)findViewById(R.id.tank_player2),2);
         Log.d(LOG_TAG, player1.toString());
         player1.setRotation(90);
         player2.setRotation(270);
@@ -169,49 +172,66 @@ public class LocalGameActivity extends AppCompatActivity {
         fireBullet(view, player2);
     }
 
+    void score(Tank player){
+        TextView scoreT;
+        if(player.getNPlayer()==1)
+            scoreT = findViewById(R.id.scoreP1);
+        else
+            scoreT = findViewById(R.id.scoreP2);
+        int score = Integer.parseInt(scoreT.getText().toString());
+        score++;
+        scoreT.setText(Integer.toString(score));
+    }
+
     //Manage the bullets fired
-    public void fireBullet(View view, Tank player) {
-        final ImageView bullet = new ImageView(this);
-        bullet.setImageResource(R.drawable.bullet1);
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                ConstraintLayout.LayoutParams.WRAP_CONTENT
-        );
-        final Collider bulletC = new Collider(true, bullet);
-        bulletC.addDoesntAffect(player);
-        CollisionSystem.getInstance().addCollider(bulletC);
+    public void fireBullet(View view, final Tank player) {
+        if(player.getLastShot()==-1 || (System.nanoTime() - player.getLastShot())/NANOS_TO_SECONDS > player.getFiringCD()) {
+            player.newShot();
+            final ImageView bullet = new ImageView(this);
+            bullet.setImageResource(R.drawable.bullet1);
+            ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+            );
+            final Collider bulletC = new Collider(true, bullet);
+            bulletC.addDoesntAffect(player);
+            CollisionSystem.getInstance().addCollider(bulletC);
 
-        //Get the angle of the tank and calculate X and Y strengths
-        float angle = player.getRotation()-90;
-        final double x = Math.cos(Math.toRadians(angle));
-        final double y = Math.sin(Math.toRadians(angle));
-        bullet.setScaleX(0.15f);
-        bullet.setScaleY(0.15f);
+            //Get the angle of the tank and calculate X and Y strengths
+            float angle = player.getRotation() - 90;
+            final double x = Math.cos(Math.toRadians(angle));
+            final double y = Math.sin(Math.toRadians(angle));
+            bullet.setScaleX(0.15f);
+            bullet.setScaleY(0.15f);
 
-        bullet.setX(player.getPosX());
-        bullet.setY(player.getPosY()+20);
-        bullet.setRotation(angle-90);
-        layout.addView(bullet, layoutParams);
+            bullet.setX(player.getPosX());
+            bullet.setY(player.getPosY() + 20);
+            bullet.setRotation(angle - 90);
+            layout.addView(bullet, layoutParams);
 
-        //Handler to create a thread to control the bullet
-        final Handler handler = new Handler();
-        final int delay = 50; //Update delay in milliseconds
+            //Handler to create a thread to control the bullet
+            final Handler handler = new Handler();
+            final int delay = 50; //Update delay in milliseconds
 
-        handler.postDelayed(new Runnable(){
-            public void run(){
-                bullet.setX((float) (bullet.getX() + x * bulletSpeedMultiplier));
-                bullet.setY((float) (bullet.getY() + y * bulletSpeedMultiplier));
+            final long startTime = System.nanoTime();
 
-                //if(!checkCollisionX(bullet.getX()) && !checkCollisionY(bullet.getY()))
-                if(!checkCollisions(bulletC.getId()))
-                    handler.postDelayed(this, delay);
-                else {
-                    Log.d(LOG_TAG, "Bullet collision");
-                    layout.removeView(bullet);
-                    CollisionSystem.getInstance().removeCollider(bulletC);
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    bullet.setX((float) (bullet.getX() + x * bulletSpeedMultiplier));
+                    bullet.setY((float) (bullet.getY() + y * bulletSpeedMultiplier));
+
+                    //if(!checkCollisionX(bullet.getX()) && !checkCollisionY(bullet.getY()))
+                    if (!checkCollisions(bulletC.getId())) {
+                        if ((System.nanoTime() - startTime) / NANOS_TO_SECONDS < MAX_BULLET_TIME) //Converting nanoseconds to seconds
+                            handler.postDelayed(this, delay);
+                    } else {
+                        Log.d(LOG_TAG, "Bullet collision");
+                        score(player);
+                        layout.removeView(bullet);
+                        CollisionSystem.getInstance().removeCollider(bulletC);
+                    }
                 }
-            }
-        }, delay);
-
+            }, delay);
+        }
     }
 }
--- app/src/main/java/io/github/eavilaes/tankwargame/Tank.java
+++ app/src/main/java/io/github/eavilaes/tankwargame/Tank.java
@@ -1,16 +1,35 @@
 package io.github.eavilaes.tankwargame;
 
-import android.util.Log;
 import android.widget.ImageView;
 
 class Tank extends Collider {
 
-    private static final String LOG_TAG = "Tank";
-
     static final float speedMultiplier = 0.1f; //Default: 0.1f
+    private int player;
+    private float firingCD;
+    private long lastShot;
 
-    Tank(ImageView imageView){
+
+    Tank(ImageView imageView, int nPlayer){
         super(true, imageView);
+        this.player=nPlayer;
+        this.firingCD =0.5f;
+        this.lastShot=-1;
     }
 
+    int getNPlayer(){
+        return this.player;
+    }
+
+    float getFiringCD(){
+        return this.firingCD;
+    }
+
+    long getLastShot(){
+        return this.lastShot;
+    }
+
+    void newShot(){
+        this.lastShot = System.nanoTime();
+    }
 }
